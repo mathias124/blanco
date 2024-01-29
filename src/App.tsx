736976e7621f485f5c35641d7023f491384c436a
s224292 <s224292@student.dtu.dk>
2024-01-29 15:30:08 +0100

    Push funny features

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
+function Ubongoet() {
+    return (
+        <>
+            <div>
+                <a href="https://www.youtube.com/" target="_blank">
+                    <img src={"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ffb340c-45b5-4137-8b12-98f2fe9645c6/de2cmfo-1e4adc56-98a4-4b9a-8cc8-bfd0046feacb.png/v1/fill/w_800,h_1471/odie__png__by_autism79_de2cmfo-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQ3MSIsInBhdGgiOiJcL2ZcLzFmZmIzNDBjLTQ1YjUtNDEzNy04YjEyLTk4ZjJmZTk2NDVjNlwvZGUyY21mby0xZTRhZGM1Ni05OGE0LTRiOWEtOGNjOC1iZmQwMDQ2ZmVhY2IucG5nIiwid2lkdGgiOiI8PTgwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.nQJRZ3EWZAjaiE9cwDb_keUpa9J2XBmnwmanrgahf5s"} className="logo" alt="Vite logo" />
+                </a>
+            </div>
+        </>
+    )
+}
 function App() {
-  const [count, setCount] = useState(0)
-    const [kage]  = ("du får kage")
-if (count ==10) {
-    kage
+  const [uBongoCounter, setUBongoCounter] = useState(0)
+
+if (uBongoCounter ==10) {
+    Ubongoet()
 }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://www.youtube.com/" target="_blank">
+          <img src={"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1ffb340c-45b5-4137-8b12-98f2fe9645c6/de2cmfo-1e4adc56-98a4-4b9a-8cc8-bfd0046feacb.png/v1/fill/w_800,h_1471/odie__png__by_autism79_de2cmfo-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQ3MSIsInBhdGgiOiJcL2ZcLzFmZmIzNDBjLTQ1YjUtNDEzNy04YjEyLTk4ZjJmZTk2NDVjNlwvZGUyY21mby0xZTRhZGM1Ni05OGE0LTRiOWEtOGNjOC1iZmQwMDQ2ZmVhY2IucG5nIiwid2lkdGgiOiI8PTgwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.nQJRZ3EWZAjaiE9cwDb_keUpa9J2XBmnwmanrgahf5s"} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://twitter.com" target="_blank">
+          <img src={"https://i.pinimg.com/originals/85/4b/78/854b78f76935bea0bf6074c70f176937.png"} className="logo react" alt="React logo" />
         </a>
-          <a href="https://www.Youtube.com/"><img src="https://www.tutorialspoint.com/assets/questions/media/426142-1668760872.png" />
-          </a>
+
 
       </div>
-      <h1>Vite + React</h1>
+      <h1>Get Garfielded my dude</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setUBongoCounter((count) => count + 1)}>
+          count is {uBongoCounter}
         </button>
         <p>
           Edit få 10 and bliv uBongoet
         </p>
+          <div>
+
+          <a href="https://twitter.com" target="_blank">
+              <img src={"https://media1.tenor.com/m/XegHfcae2QEAAAAd/bonk-dog.gif"}/>
+          </a>
+
+      </div>
 
       </div>
         <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
+            Click on the shit and learn more!
         </p>
     </>
   )
 }
+
+
 
 export default App
